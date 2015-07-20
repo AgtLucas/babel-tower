9e1b961c7fcbd14ee06cebaf654b4207afc116f1
Lucas <lucas@agtlucas.com>
2015-07-20 08:21:26 -0300

    Fix typo

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ var bob = {
   _name: 'Bob',
   _friends: [],
   printFriends() {
-    this._friends.forEach(f => console.log(this._name + 'knows ' f));
+    this._friends.forEach(f => console.log(this._name + 'knows ' + f));
   }
 }
