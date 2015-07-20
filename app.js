fbfc0700df452f36c99b4bb56071b17b8b979e7f
Lucas <lucas@agtlucas.com>
2015-07-20 08:21:03 -0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+var odds = evens.map(v => v + 1);
+var nums = evens.map((v, i) => v + 1);
+
+nums.forEach(v => {
+  if (v % 5 === 0)
+    fives.push(v);
+});
+
+var bob = {
+  _name: 'Bob',
+  _friends: [],
+  printFriends() {
+    this._friends.forEach(f => console.log(this._name + 'knows ' f));
+  }
+}
