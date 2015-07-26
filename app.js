b90932733b208ccc8b6ad7d93a59845fb29791e3
Lucas <lucas@agtlucas.com>
2015-07-26 17:53:42 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,1 @@
 'use strict';
-
-var odds = evens.map(v => v + 1);
-var nums = evens.map((v, i) => v + 1);
-
-nums.forEach(v => {
-  if (v % 5 === 0)
-    fives.push(v);
-});
-
-var bob = {
-  _name: 'Bob',
-  _friends: [],
-  printFriends() {
-    this._friends.forEach(f => console.log(this._name + 'knows ' + f));
-  }
-}
